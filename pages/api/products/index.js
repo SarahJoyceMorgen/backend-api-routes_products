2b6016822cff7c549608422093fc36f1e818aa69
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-19 16:30:29 +0100

    export default function ProductDetails

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,6 +1,32 @@
 import { getAllProducts } from "@/services/productServices";
+import useSWR from 'swr';
 
 export default function handler(request, response) {
     const products = getAllProducts();
     res.status(200).json(products);
+}
+
+const fetcher = async (url) => {
+    const res = await fetch(url);
+    return res.json();
+  };
+
+
+  export default function ProductDetail() {
+    const router = useRouter();
+    const { id } = router.query;
+
+    const { data, error} =useSWR(`/api/products/${id}`, fetcher)
+
+    if (error) return <div>Oops...Error!</div>
+    if (!data) return <div>Loading...</div>
+
+    return(
+        <div>
+            <p>Product Details</p>
+            <p>Name: {data.name}</p>
+            <p>Price: {data.price}</p>
+        </div>
+    )
+
 }
